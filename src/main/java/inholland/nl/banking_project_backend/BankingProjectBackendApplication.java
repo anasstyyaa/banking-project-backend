fbe07f6f5eaad8a5bf1cca5ba4540f44ab4daa82
anasstyyaa <734831@student.inholland.nl>
2026-04-20 19:38:12 +0200

    set up

---
+++ src/main/java/inholland/nl/banking_project_backend/BankingProjectBackendApplication.java
@@ -0,0 +1,13 @@
+package inholland.nl.banking_project_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BankingProjectBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BankingProjectBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/inholland/nl/banking_project_backend/BankingProjectBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package inholland.nl.banking_project_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BankingProjectBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
